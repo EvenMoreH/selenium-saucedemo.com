530703aaf9511b2e92d3d5ebeb0ee887bf9df6a2
EvenMoreH <hubert.lewan@gmail.com>
2025-07-28 18:56:11 +0200

    Updated Cart POM and added test for individual product add/remove from cart on cart page.

--- pages/cart_page.py
+++ pages/cart_page.py
@@ -10,3 +10,18 @@ class Cart:
 
     def return_to_products_page(self):
         self.driver.find_element(*self.continue_shopping_btn).click()
+
+    def is_in_cart(self, product_id: str):
+        try:
+            remove_from_cart_button = (By.ID, f"remove-{product_id}")
+            # returning if remove button is displayed
+            return self.driver.find_element(*remove_from_cart_button).is_displayed()
+        except NoSuchElementException:
+            return False
+
+    def remove_from_cart(self, product_id: str):
+        try:
+            remove_from_cart_button = (By.ID, f"remove-{product_id}")
+            self.driver.find_element(*remove_from_cart_button).click()
+        except NoSuchElementException:
+            return False
--- tests/test_cart.py
+++ tests/test_cart.py
@@ -2,6 +2,8 @@ import pytest
 from utils.config import BASE_URL
 from utils.browser_setup import driver
 from pages.cart_page import Cart
+from pages.products_page import ProductsPage
+from utils.product_data import PRODUCT_IDS
 
 
 @pytest.mark.cart
@@ -21,5 +23,25 @@ def test_continue_shopping_button(open_cart_page):
     )
 
 @pytest.mark.cart
-def test_remove_from_cart_on_cart_page():
-    pass
+@pytest.mark.parametrize("product_id", PRODUCT_IDS)
+def test_remove_from_cart_on_cart_page(var_user_logged, product_id):
+    current_user, driver = var_user_logged
+    products_page = ProductsPage(driver)
+    cart_page = Cart(driver)
+
+    # Step 1: Add product and go to cart
+    products_page.add_to_cart(product_id)
+    products_page.open_cart()
+
+    # Step 2: Check if items were added
+    assert cart_page.is_in_cart(product_id), (
+        f"Expected {product_id} to be in cart, but {current_user} could not add it. "
+    )
+
+    # Step 3: Remove items
+    cart_page.remove_from_cart(product_id)
+
+    # Step 4: Check if items were removed correctly
+    assert not cart_page.is_in_cart(product_id), (
+        f"Expected {product_id} to be removed from cart but it is still present. "
+        )
